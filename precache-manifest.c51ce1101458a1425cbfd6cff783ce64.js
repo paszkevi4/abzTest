1f1a8bf24ed122d4524fa9b94b752935958493ef
paszkevi4 <paszkevi4@aol.com>
2020-05-05 10:21:55 +0300

    Updates

---
+++ precache-manifest.c51ce1101458a1425cbfd6cff783ce64.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1fd5e73ab6590ba68797d6e202e8de",
+    "url": "/abzTest/index.html"
+  },
+  {
+    "revision": "c01b968b9ed1021b9f5e",
+    "url": "/abzTest/static/css/main.76c6c2b7.chunk.css"
+  },
+  {
+    "revision": "62a7207d1d6ac69d455c",
+    "url": "/abzTest/static/js/2.a4a75652.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/abzTest/static/js/2.a4a75652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01b968b9ed1021b9f5e",
+    "url": "/abzTest/static/js/main.d9238176.chunk.js"
+  },
+  {
+    "revision": "cb35614e0d58077d93bc",
+    "url": "/abzTest/static/js/runtime-main.200f44a2.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/abzTest/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/abzTest/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/abzTest/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/abzTest/static/media/menu-icon.6f6bef28.svg"
+  },
+  {
+    "revision": "3a41f082d05e2a2756c67f67f21b2324",
+    "url": "/abzTest/static/media/photo-cover.3a41f082.svg"
+  }
+]);
